acb86f84ecec7bb9dcf76a06dd7ab81e5ff5bfd3
bldecherf <bdecherf074@gmail.com>
2015-04-01 14:19:09 +0200

    testons des trucs

--- plugins/galette-plugin-aae/arbre.php
+++ plugins/galette-plugin-aae/arbre.php
@@ -22,10 +22,98 @@ if ( !$preferences->showPublicPages($login) ) { //$login->isLogged())
     die();
 }
 
-$annuaire = new Annuaire();
+
 $cycles = new Cycles();
+$annuaire = new Annuaire();
 
+//Recuperation cycles
+$allCycles = $cycles->getAllCycles();
+foreach ($allCycles as $key => $cycle) {
+	$tmp[$key] = $cycle["nom"];
+}
 
+//Tri ascendant
+array_multisort($tmp, SORT_ASC, $allCycles);
+$tpl->assign('cycles', $allCycles);
+
+if(($nom!='')||($prenom!='')){
+	$eleves = $annuaire -> getPromotion($cycle,$year);
+}
+else{
+	//search among "ingenieurs"
+	$id_cycle_simple = 51;//starting year
+	//$annee_debut = get_numeric_form_value('annee_debut', '');//starting year
+	$tri=$_POST['tri'];
+	//$tpl->assign('id_cycle', $id_cycle);
+	
+	//If there is a name
+	if ($_POST["nom"]!="")
+	{	
+		$researched_name=$_POST["nom"];
+		
+		//Text to uppercase
+		$researched_name = strtoupper($researched_name);
+		
+		//Get all students name
+		$studentsName = $annuaire->getNameOfAllStudents();
+		
+		//Récupération du nom le plus proche
+		$found_name=$annuaire->proximite_levenshtein($researched_name,$studentsName);
+	};
+	
+	if ($_POST["prenom"]!="")
+	{
+		$researched_surname=$_POST["prenom"];
+		
+		//Transforme le prenom en minuscule
+		$researched_surname = strtolower($researched_surname);
+		
+		//Transforme la premièrer lettre en majuscule
+		$researched_surname[0] = strtoupper($researched_surname[0]);
+		
+		//Creation d'un tableau contenant les prénoms de chaque eleve
+		$studentsSurname= $annuaire->getSurnameOfAllStudents();
+	
+		//Récupération du nom le plus proche
+		$found_surname=$annuaire->proximite_levenshtein($researched_surname,$studentsSurname);
+		
+	};
+	
+	$param_selected = ((($id_cycle != '') && ($annee_debut != '')) || $found_name!=NULL || $found_surname!=Null);
+	
+	if($param_selected) {
+		
+		if($id_cycle_simple==''){
+			
+			$eleves = $annuaire -> getStudent($found_name,$found_surname);
+		}
+		else{
+		
+			$eleves = $annuaire -> getStudent($found_name,$found_surname);
+			
+		}
+	}
+}	
+// Obtient une liste de colonnes
+foreach ($eleves as $key => $row) {
+	$id_adh[$key]=$row['id_adh'];
+	$nom[$key]  = $row['nom_adh'];
+	$prenom[$key] = $row['prenom_adh'];
+	$cycletab[$key] = $row['nom'];
+	$promo[$key] = $row['annee_debut'];
+	$id_cycle[$key] = $row['id_cycle'];
+}
+	
+// Trie les données par nom et prenom croissant
+// Ajoute $eleves en tant que dernier paramètre, pour trier par la clé commune
+array_multisort($nom, SORT_ASC, $prenom, SORT_ASC, $eleves);	
+$tpl->assign('eleves', $eleves);
+$tpl->assign('nb_eleves', count($eleves));
+$tpl->assign('tri',$tri);
+$tpl->assign('page_title', _T("Former students list"));
+$tpl->assign('param_selected', $param_selected);
+$tpl->assign('id_cycle_simple', $id_cycle_simple);
+$tpl->assign('annee_debut', $annee_debut);
 
 //Set the path to the current plugin's templates,
 //but backup main Galette's template path before
