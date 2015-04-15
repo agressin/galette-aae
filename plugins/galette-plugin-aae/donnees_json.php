14704bd964ba026f5e1e49a8e11b9f5959cefa1c
SGuinard <st.guinard@gmail.com>
2015-04-15 16:48:34 +0200

    en plus on prend les annees deb et fin

--- plugins/galette-plugin-aae/donnees_json.php
+++ plugins/galette-plugin-aae/donnees_json.php
@@ -20,9 +20,11 @@ $nom = 'GUINARD';
 $prenom = 'Stéphane';
 
 $array = [];
+$annee_debut = 9999;
+$annee_fin = 0;
 
 $array_points = $annuaire->getStudent($nom,$prenom);
-
+//var_dump($array_points[0][id_adh]); //Affiche l'id d'une personne
 //Récupération des parrains
 $id_fillot1 = 1847;
 
@@ -34,14 +36,23 @@ do  {
 	//Pour chaque parrain on chercher ses parrains
 	foreach ($idparrain as $cle => $valeur) 
 			{
+				
+				//Recuperation des annees debut
+				$infos = [];
+				$infos = $annuaire->getInfoById($valeur);
+				$annee_deb = $infos[0][annee_debut];
+				if ($annee_deb < $annee_debut){
+					$annee_debut = $annee_deb;
+				}
 				$idparrain2=getParrains($valeur);//On stocke les parrains
 				$id_parrain[] = $valeur;//On les met dans un tableau qui contiendra tous les parrains les ns à la suite des autres
 			}
-			$idparrain = $idparrain2;//On commence une nouvelle boucle avec els nouveaux parrains obtenus
+			$idparrain = $idparrain2;//On commence une nouvelle boucle avec les nouveaux parrains obtenus
 } while (count($idparrain) > 0);
 
 
-var_dump($id_parrain); //Affichage
+//var_dump($id_parrain); //Affichage
+//echo($annee_debut);
 
 
 echo(' ');
@@ -54,14 +65,21 @@ $idfillot2 = [];
 do  {
 	foreach ($idfillot as $cle => $valeur) 
 			{
+				//Recuperation des annees fin
+				$infos = [];
+				$infos = $annuaire->getInfoById($valeur);
+				$annee_finale = $infos[0][annee_debut];
+				if ($annee_finale > $annee_fin){
+					$annee_fin = $annee_finale;
+				}
 				$idfillot2=getFillots($valeur);
 				$id_fillot[] = $valeur;
 			}
 			$idfillot = $idfillot2;
 } while (count($ifillot) > 0);
 
-
-var_dump($id_fillot);
+//echo($annee_fin);
+//var_dump($id_fillot);
 
 //Construction type du JSON
 $json_points = json_encode($array_points, JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE);
