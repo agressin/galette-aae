1ae91d9fdae080d42c37e5f845a3385c46844357
SGuinard <st.guinard@gmail.com>
2015-04-24 15:17:01 +0200

    Fallait aussi penser aux fillots ^^'

--- plugins/galette-plugin-aae/donnees_json.php
+++ plugins/galette-plugin-aae/donnees_json.php
@@ -68,7 +68,7 @@ do  {
 						$idparrain2=array_merge($idparrain2,getParrains($valeur));//On stocke les parrains
 						$idparrain3 = getParrains($valeur);
 						//var_dump($idparrain3);
-						echo(" ");
+						//echo(" ");
 						/*foreach ($idparrain2 as $cle => $valeur){
 							echo ($valeur);
 							echo(" ");
@@ -86,7 +86,7 @@ do  {
 					//$idparrain2 = [];
 					//$idparrain2[] = $idh;
 					//echo(sizeOf($idparrain));
-					echo("blabla");
+					//echo("blabla");
 					$nodes = $nodes.'{"data":{"id":"'.$idh.'","name":"'.$idh.'"}},';
 					$edges = $edges.'{"data":{"id":"'.$idh.'","source":"'.$idh.'","target":"'.$valeur.'"}},';
 					$idh = $idh - 1;
@@ -142,34 +142,91 @@ $idfillot2 = [];
 $idp = $id_fillot1;
 
 do  {
+	$idfillot2 = [];
+	$idfillot3 = [];
+	//Pour chaque parrain on chercher ses parrains
 	foreach ($idfillot as $cle => $valeur) 
 			{
-				//Recuperation des annees fin
+				//echo($idparrain[0]." ");
+				//echo($valeur);
+				//Recuperation des annees debut
 				$infos = [];
 				$infos = $annuaire->getInfoById($valeur);
 				if (strlen ($infos[0][nom])>3){ //Si jamais les infos sur le parrain sont liées à son cursus IT3, in enlève 2 ans à son année d'entrée pour savoir quand il et arrivé à l'école
 					$infos[0][annee_debut] = $infos[0][annee_debut] - 2;
 				}
-				$annee_finale = $infos[0][annee_debut];
-				if ($annee_finale > $annee_fin){
-					$annee_fin = $annee_finale;
+				//var_dump($infos[0][nom]);
+				$annee_deb = $infos[0][annee_debut];
+				if ($annee_deb < $annee_debut){
+					$annee_debut = $annee_deb;
 				}
-				$idfillot2=getFillots($valeur);
-				$id_fillot[] = $valeur;
-				$nodes = $nodes.'{"data":{"id":"'.$idn.'","name":"'.$infos[0][prenom_adh]/*.'" "'.$infos[0][nom_adh]*/.'"}},';
-				$idn = $idn+1;
-				$idvieux = $idn-1;
-				if ($idp != $id_fillot1){
-					$edges = $edges.'{"data":{"id":"'.$ide.'","source":"'.$idp.'","target":"'.$idvieux.'"}},';
-					$ide = $ide+1;
+				if (sizeOf(getFillots($valeur))>0){
+					//for (i=1:sizeOf(getParrains($valeur))){
+						$idfillot2=array_merge($idfillot2,getFillots($valeur));//On stocke les parrains
+						$idfillot3 = getFillots($valeur);
+						//var_dump($idparrain3);
+						//echo(" ");
+						/*foreach ($idparrain2 as $cle => $valeur){
+							echo ($valeur);
+							echo(" ");
+						}*/
+					//}
 				}
 				else {
-					$edges = $edges.'{"data":{"id":"'.$ide.'","source":"9999","target":"'.$idvieux.'"}},';
-					$ide = $ide+1;
+					$idfillot3 = [];
 				}
+				/*if (empty($idfillot3)/*&&*sizeOf($idparrain)!=1 && sizeOf($idparrain2) == 0*///){
+					//var_dump($idparrain2);
+					/*$nodes = $nodes.'{"data":{"id":"'.$valeur.'","name":"'.$infos[0][prenom_adh]/*.'" "'.$infos[0][nom_adh]*///.'"}},';
+					/*$idn = $idn+1;
+					$idvieux = $idn-1;
+					//$idparrain2 = [];
+					//$idparrain2[] = $idh;
+					//echo(sizeOf($idparrain));
+					//echo("blabla");
+					$nodes = $nodes.'{"data":{"id":"'.$idh.'","name":"'.$idh.'"}},';
+					$edges = $edges.'{"data":{"id":"'.$idh.'","source":"'.$idh.'","target":"'.$valeur.'"}},';
+					$idh = $idh - 1;
+				}*/
+				//TODO : pour les redoublants : insérer un élément vide entre eux et leur fillot
+				/*else if ($infos[0][annee_fin] - $infos[0][annee_fin] == 4){
+					$nodes = $nodes."{data:{id:".$idn.",name:".$infos[0][prenom_adh]." ".$infos[0][nom_adh]."}},";
+					$idn = $idn+1;
+					$idvieux = $idn-1;
+					$nodes = $nodes."{data:{id:".$idh.",name:".$infos[0][prenom_adh]." ".$infos[0][nom_adh]."}},";
+					$edges = $edges."{data:{id:".$ide.",source:".$idh.",target:$idvieux}},";
+					$ide = $ide+1;
+					$idh = $idh-1;
+				}*/
+				//else {
+					$nodes = $nodes.'{"data":{"id":"'.$valeur.'","name":"'.$infos[0][prenom_adh]/*.'" "'.$infos[0][nom_adh]*/.'"}},';
+					$idn = $idn+1;
+					$idvieux = $idn-1;
+					//echo($idvieux);
+					if ($idp != $id_fillot1){
+						foreach ($idfillot3 as $cle => $nouveaufillot){
+							echo($nouveaufillot);
+							$edges = $edges.'{"data":{"id":"'.$ide.'","source":"'.$valeur.'","target":"'.$nouveaufillot.'"}},';
+							$ide = $ide+1;
+						}
+						
+					}
+					else {
+						//foreach ($idparrain2 as $cle => $nouveauparrain){
+							$edges = $edges.'{"data":{"id":"'.$ide.'","source":"9999","target":"'.$valeur.'"}},';
+							$ide = $ide+1;
+							foreach ($idfillot3 as $cle => $nouveaufillot){
+								$edges = $edges.'{"data":{"id":"'.$ide.'","source":"9999","target":"'.$nouveaufillot.'"}},';
+								$ide = $ide+1;
+							}
+						//}
+						//echo($idvieux);
+					}
+				//}
 			}
 			$idp = $idvieux;
-			$idfillot = $idfillot2;
+			$idfillot = $idfillot2;//On commence une nouvelle boucle avec les nouveaux parrains obtenus
+			$idfillot2 = [];
 } while (count($idfillot) > 0);
 
 $layout = '"layout": {"name": "breadthfirst", "directed": true, "roots":"#8772", "padding": 10} }';
