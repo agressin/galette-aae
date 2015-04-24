2e6387d53ba10e7f08c64a6e2557c49c939fd9f5
A Gressin <adrien.gressin@gmail.com>
2015-04-24 12:14:10 +0200

    Revert "le tout"
    This reverts commit 3c27eaf77a92996ee962ce9fff27589fcd9fce3e.

--- config/config.inc.php
+++ config/config.inc.php
@@ -2,7 +2,9 @@
 define("TYPE_DB", "mysql");
 define("HOST_DB", "localhost");
 define("PORT_DB", "3306");
-define("USER_DB", "root");
-define("PWD_DB", "root");
+define("USER_DB", "aaeensgeoqtest");
+define("PWD_DB", "NH9R9szNvzNZ");
 define("NAME_DB", "aaeensgeoqtest");
 define("PREFIX_DB", "galette_");
+define("STOCK_FILES", "tempimages");
+?>
--- plugins/galette-plugin-aae/liste_eleves.php
+++ plugins/galette-plugin-aae/liste_eleves.php
@@ -6,12 +6,11 @@ require_once GALETTE_BASE_PATH . 'includes/galette.inc.php';
 //Constants and classes from plugin
 require_once '_config.inc.php';
 
+require_once 'lib/GaletteAAE/Formations.php';
+use Galette\AAE\Formations as Formations;
+
 require_once 'lib/GaletteAAE/Cycles.php';
 use Galette\AAE\Cycles as Cycles;
-
-require_once 'lib/GaletteAAE/Annuaire.php';
-use Galette\AAE\Annuaire as Annuaire;
-
 
 if ( !$preferences->showPublicPages($login) ) {
     //public pages are not actives
@@ -19,115 +18,54 @@ if ( !$preferences->showPublicPages($login) ) {
     die();
 }
 
+$formation = new Formations();
 $cycles = new Cycles();
-$annuaire = new Annuaire();
 
-//Recuperation cycles
+//Recup cycle
 $allCycles = $cycles->getAllCycles();
 foreach ($allCycles as $key => $cycle) {
 	$tmp[$key] = $cycle["nom"];
 }
-//Tri ascendant
+//Tri
 array_multisort($tmp, SORT_ASC, $allCycles);
 $tpl->assign('cycles', $allCycles);
 
-//We get variables
-$id_cycle = get_numeric_form_value('id_cycle', '');//cycle id
-$id_formation = get_numeric_form_value('id_formation', '');//formation id same as cycle id but for a faster research
-$annee_debut = get_numeric_form_value('annee_debut', '');//starting year
-$tri=$_POST['tri'];
-$tpl->assign('id_cycle', $id_cycle);
+$simple = !empty($_POST['id_cycle_simple']);
+if($simple) {
+	$id_cycle_simple = $_POST['id_cycle_simple'];
+}
+$id_cycle = get_numeric_form_value('id_cycle', '');
+$annee_debut = get_numeric_form_value('annee_debut', '');
 
-//If there is a name
-if ($_POST["nom"]!="")
-{	
-	$researched_name=$_POST["nom"];
-	
-	//Text to uppercase
-	$researched_name = strtoupper($researched_name);
-	
-	//Get all student names
-		$students = $annuaire->getNameOfAllStudents();
-		
-		//Get colums with students names
-			foreach ($students as $cle => $valeur) 
-			{
-				$nom[$cle]  = $valeur['nom_adh'];
-			};
-
-		//Sort table 
-		//Add $student to sort by same key
-			array_multisort($nom, SORT_ASC, $students);
-
-		//Table with all names 
-			//Initialisation du tableau
-			$listOfStudents=array();
-			
-			//Remplissage du tableau
-			foreach ($students as $cle => $valeur) 
-			{
-				$listOfStudents[]=$valeur['nom_adh'];
-			}
-			
-	/*Récupération du nom le plus proche*/
-		$found_name=$annuaire->proximite_levenshtein($researched_name,$listOfStudents);
-	
-};
-
-/*recherche du prénom si la recherche a été effectuée*/
-if ($_POST["prenom"]!="")
-{
-	$researched_surname=$_POST["prenom"];
-	
-	//Transforme le prenom en minuscule
-	$researched_surname = strtolower($researched_surname);
-	//Transforme la premièrer lettre en majuscule
-	$researched_surname[0] = strtoupper($researched_surname[0]);
-	
-	/*Creation d'un tableau contenant les prénoms de chaque eleve*/
-		$prenomStudents= $annuaire->getSurnameOfAllStudents();
-		
-		// Obtiention d'une liste de colonnes
-			foreach ($prenomStudents as $cle => $valeur) 
-			{
-				$prenom[$cle]  = $valeur['prenom_adh'];
-			};
-
-		/*Tri des données par prénom croissant*/
-		/* Ajout de $prenomStudents en tant que dernier paramètre, pour trier par la clé commune*/
-			array_multisort($prenom, SORT_ASC, $prenomStudents);
-
-		/*Tableau contenant les noms de chaque eleve (pour utiliser la distance de levenshtein)*/
-			//Initialisation du tableau
-			$listOfStudentsSurname=array();
-			
-			//Remplissage du tableau
-			foreach ($prenomStudents as $cle => $valeur) 
-			{
-				$listOfStudentsSurname[]=$valeur['prenom_adh'];
-			};
-
-	/*Récupération du nom le plus proche*/
-		$found_surname=$annuaire->proximite_levenshtein($researched_surname,$listOfStudentsSurname);
-	
-};
-
-
-$param_selected = ((($id_cycle != '') && ($annee_debut != '')) || $found_name!=NULL || $found_surname!=Null);
+$param_selected = ($simple || ($id_cycle != '') ) && ($annee_debut != '');
 
 if($param_selected) {
-		
-		$eleves = $annuaire -> getStudent($found_name,$found_surname,$annee_debut,$id_formation,$id_cycle);
+	
+	if($simple){
+	
+		switch ($id_cycle_simple) {
+		case "IT":
+		    $eleves = $formation->getIngPromotion($annee_debut);
+		    break;
+		case "G":
+		    $eleves = $formation->getGPromotion($annee_debut);
+		    break;
+		case "DC":
+		    $eleves = $formation->getDCPromotion($annee_debut);
+		    break;
+		}
+	//
+	
+	} else{
+		$eleves = $formation->getPromotion($id_cycle,$annee_debut);
+	}
+	
 
 
 	// Obtient une liste de colonnes
 	foreach ($eleves as $key => $row) {
-		$id_adh[$key]=$row['id_adh'];
 		$nom[$key]  = $row['nom_adh'];
 		$prenom[$key] = $row['prenom_adh'];
-		$cycletab[$key] = $row['nom'];
-		$promo[$key] = $row['annee_debut'];
-		$id_cycle[$key] = $row['id_cycle'];
 	}
 
 	// Trie les données par nom et prenom croissant
@@ -136,10 +74,11 @@ if($param_selected) {
 
 	$tpl->assign('eleves', $eleves);
 	$tpl->assign('nb_eleves', count($eleves));
-};
-$tpl->assign('tri',$tri);
+}
+
 $tpl->assign('page_title', _T("Former students list"));
 $tpl->assign('param_selected', $param_selected);
+$tpl->assign('id_cycle', $id_cycle);
 $tpl->assign('id_cycle_simple', $id_cycle_simple);
 $tpl->assign('annee_debut', $annee_debut);
 
